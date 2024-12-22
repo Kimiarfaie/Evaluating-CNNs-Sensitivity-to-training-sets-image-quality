030bb81c5d10e31d83eb37f62bcd28fa8ae86a0b
Kimiarfaie <168003992+Kimiarfaie@users.noreply.github.com>
2024-12-22 15:31:24 +0330

    Delete Distort Images /code_imdistort directory

--- Distort Images /code_imdistort/imcolorsaturate.m
+++
@@ -1,8 +0,0 @@
-function [b] = imcolorsaturate(a,factor)
-
-hsvIm = rgb2hsv(a);
-hsvIm(:,:,2) = hsvIm(:,:,2) * factor;
-hsvIm(:,:,2) = min(max(hsvIm(:,:,2) * factor, 0), 1);
-b = hsv2rgb(hsvIm);
-
-end
